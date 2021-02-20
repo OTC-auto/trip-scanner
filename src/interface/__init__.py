7f2139f17030308e0dd5bcfc5b0b7659916f03ab
OTC-auto <hello.dennis@hotmail.com>
2021-02-20 19:42:34 +0000

    fix: English grammer

--- src/interface/__init__.py
+++ src/interface/__init__.py
@@ -3,14 +3,14 @@ from time import sleep
 
 def main():
     while True:
-        query = input("Type 'scan' and a CSV with trips data will be created on your Desktop.")
+        query = input('Type "scan" and a CSV with trips data will be created on your Desktop.')
         if query.lower() == 'scan':
             pass  # run program
             print(f"File created. Shutting down this program.")
             sleep(2)
             break
         else:
-            print(f"You typed: {query}. Please type 'scan'.")
+            print(f'You typed: {query}. Please type "scan".')
 
 
 if __name__ == '__main__':
